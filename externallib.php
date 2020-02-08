4b39115eb7a7d15f89e2b55c80589e59dc5eebb6
Luca Bösch <luca.boesch@bfh.ch>
2020-02-08 13:48:49 +0100

    Changed params away from welcomemessage, originally from the template.

--- externallib.php
+++ externallib.php
@@ -1,4 +1,6 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
 // Moodle is free software: you can redistribute it and/or modify
 // it under the terms of the GNU General Public License as published by
 // the Free Software Foundation, either version 3 of the License, or
@@ -19,6 +21,9 @@
  * @copyright   2020 Nina Herrmann <nina.herrmann@uni-muenster.de> Luca Bösch <luca.boesch@bfh.ch>
  * @license     http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+
+defined('MOODLE_INTERNAL') || die();
+
 require_once($CFG->libdir . "/externallib.php");
 
 /**
@@ -32,35 +37,39 @@ class local_ws_fileassistant_external extends external_api {
      */
     public static function create_file_resource_parameters() {
         return new external_function_parameters(
-            array('welcomemessage' => new external_value(PARAM_TEXT, 'The welcome message. By default it is "Hello world,"', VALUE_DEFAULT, 'Hello world, '))
+            array('filename' => new external_value(PARAM_TEXT, 'A path to a file in a user\'s \'private files\', ' .
+                  'including the path', VALUE_OPTIONAL))
         );
     }
 
     /**
-     * Returns welcome message
+     * Assists with a file operation.
      *
-     * @param string $welcomemessage welcome message first words
-     * @return string welcome message
+     * @param string $filename file name including path
+     * @return array $results The array of results.
      */
-    public static function create_file_resource($welcomemessage = 'Hello world, ') {
+    public static function create_file_resource($filename) {
         global $USER;
 
-        //Parameter validation
-        //REQUIRED
+        // Parameter validation.
+        // REQUIRED.
         $params = self::validate_parameters(self::create_file_resource_parameters(),
-            array('welcomemessage' => $welcomemessage));
+            array('filename' => $filename));
 
-        //Context validation
+        // For sure: filename courseid sectionnumber action.
+        // Maybe: alias display intro printintro popupwidth popupheight showsize showtype showdate.
+
+        // Context validation.
         $context = \context_user::instance($USER->id);
         self::validate_context($context);
 
-        //Capability checking
-        //OPTIONAL but in most web service it should present
+        // Capability checking.
+        // OPTIONAL but in most web service it should present.
         if (!has_capability('repository/user:view', $context)) {
             throw new moodle_exception('cannotviewprofile');
         }
 
-        return $params['welcomemessage'] . $USER->firstname ;;
+        return $params['filename'] . $USER->firstname;
     }
 
     /**
@@ -68,6 +77,6 @@ class local_ws_fileassistant_external extends external_api {
      * @return external_description
      */
     public static function create_file_resource_returns() {
-        return new external_value(PARAM_TEXT, 'The welcome message + user first name');
+        return new external_value(PARAM_TEXT, 'Filename + user first name');
     }
 }
